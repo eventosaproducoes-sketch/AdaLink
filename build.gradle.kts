a41c41a60976a48dbc760e53f4959a4bd1c1189d
Museu Pensadores Universais <eventosaproducoes@gmail.com>
2026-09-13 20:18:23 -0300

    Upgrade Android Gradle plugin and Kotlin version
    Updated Android Gradle plugin and Kotlin version.

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,26 +1,4 @@
 plugins {
-    id("com.android.application")
-    id("org.jetbrains.kotlin.android")
-}
-
-android {
-    namespace = "com.adalink"
-    compileSdk = 35
-
-    defaultConfig {
-        applicationId = "com.adalink"
-        minSdk = 26
-        targetSdk = 35
-        versionCode = 1
-        versionName = "0.1"
-    }
-
-    compileOptions {
-        sourceCompatibility = JavaVersion.VERSION_17
-        targetCompatibility = JavaVersion.VERSION_17
-    }
-
-    kotlinOptions {
-        jvmTarget = "17"
-    }
+    id("com.android.application") version "8.5.2" apply false
+    id("org.jetbrains.kotlin.android") version "2.0.21" apply false
 }
