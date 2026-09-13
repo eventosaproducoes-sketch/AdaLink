35145dc03946e111e1013fdb14f03a500375a2a2
Museu Pensadores Universais <eventosaproducoes@gmail.com>
2026-09-13 19:01:02 -0300

    Configura aplicativo AdaLink V0.1
    Added Android application configuration with Kotlin support.

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,1 +1,17 @@
+plugins {
+    id("com.android.application")
+    id("org.jetbrains.kotlin.android")
+}
 
+android {
+    namespace = "com.adalink"
+    compileSdk = 35
+
+    defaultConfig {
+        applicationId = "com.adalink"
+        minSdk = 26
+        targetSdk = 35
+        versionCode = 1
+        versionName = "0.1"
+    }
+}
