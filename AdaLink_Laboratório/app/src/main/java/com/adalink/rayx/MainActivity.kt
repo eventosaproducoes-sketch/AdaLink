d52af09aec8dd9d84df344c7b57acf815843f5dc
Museu Pensadores Universais <eventosaproducoes@gmail.com>
2026-09-14 06:54:01 -0300

    Fix formatting in MainActivity.kt

--- AdaLink_Laboratório/app/src/main/java/com/adalink/rayx/MainActivity.kt
+++ AdaLink_Laboratório/app/src/main/java/com/adalink/rayx/MainActivity.kt
@@ -20,7 +20,7 @@ class MainActivity : Activity() {
             ==============================
 
             📱 SISTEMA
-            ${Sistema.texto()}
+            
 
             ==============================
 
