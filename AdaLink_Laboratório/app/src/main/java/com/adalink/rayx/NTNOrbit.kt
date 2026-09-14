69ccb6f54c5ce5d6d6f197ad844c4d6b463c71ca
Museu Pensadores Universais <eventosaproducoes@gmail.com>
2026-09-14 16:14:50 -0300

    Update NTNOrbit.kt

--- AdaLink_Laboratório/app/src/main/java/com/adalink/rayx/NTNOrbit.kt
+++ AdaLink_Laboratório/app/src/main/java/com/adalink/rayx/NTNOrbit.kt
@@ -22,25 +22,12 @@ object NTNOrbit {
 
         val x = r * cos(angulo)
         val y = r * sin(angulo)
-        val z = 0.0
+                val z = 0.0
 
         return doubleArrayOf(x, y, z)
-      fun velocidadeOrbital(): Double {
-    val r = EARTH_KM + ALTITUDE_KM
-    return sqrt(MU / r)
-}
+    }
 
-fun distanciaCentroTerra(
-    tempoSeg: Double
-): Double {
-
-    val p = posicao(tempoSeg)
-
-    return sqrt(
-        p[0] * p[0] +
-        p[1] * p[1] +
-        p[2] * p[2]
-    )
-}
-
+    fun velocidadeOrbital(): Double {
+        val r = EARTH_KM + ALTITUDE_KM
+        return sqrt(MU / r)
     }
