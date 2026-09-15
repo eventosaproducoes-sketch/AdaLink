ea21b213e0253b30c5ee102b4563e3100e790ef3
Museu Pensadores Universais <eventosaproducoes@gmail.com>
2026-09-15 15:16:52 -0300

    Add connectivity scanner text to MainActivity

--- AdaLink_Laboratório/app/src/main/java/com/adalink/rayx/MainActivity.kt
+++ AdaLink_Laboratório/app/src/main/java/com/adalink/rayx/MainActivity.kt
@@ -82,6 +82,8 @@ class MainActivity : Activity() {
 
         r.append(Conectividade.texto(this))
         r.append("\n")
+        r.append(ConnectivityScanner.texto(this))
+        r.append("\n")
 
         r.append(WifiBluetooth.texto(this))
         r.append("\n")
