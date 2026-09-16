046a056fc71868297a3a6e8cb07b3cb925a11d98
Museu Pensadores Universais <eventosaproducoes@gmail.com>
2026-09-15 22:07:02 -0300

    Update MainActivity.kt

--- AdaLink_Laboratório/app/src/main/java/com/adalink/rayx/MainActivity.kt
+++ AdaLink_Laboratório/app/src/main/java/com/adalink/rayx/MainActivity.kt
@@ -82,8 +82,7 @@ class MainActivity : Activity() {
 
         r.append(Conectividade.texto(this))
         r.append("\n")
-        r.append(RadioEnvironment.texto(this))
-        r.append("\n")
+        
         r.append(WifiBluetooth.texto(this))
         r.append("\n")
         r.append(SatelliteHunter.texto(tm))
